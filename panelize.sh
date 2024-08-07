ec8c719b6505deb3b39d2c917875b9ec7569a162
ocanath <ocanath@gmail.com>
2024-08-07 11:20:53 -0700

    panel order

---
+++ panelize.sh
@@ -0,0 +1,6 @@
+kikit panelize \
+    --layout 'grid; rows: 3; cols: 3; space: 2mm' \
+    --tabs 'fixed; width: 2mm' \
+    --cuts 'mousebites; drill: 0.4mm; spacing: 0.55mm; offset: -0.3mm' \
+    --post 'millradius: 1mm' \
+    can-b2b-interconnect.kicad_pcb panel.kicad_pcb
